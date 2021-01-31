e0974116fa42c4debfd5dd08b8335856041bd9be
Andrew Downs <andrewdownsau@gmail.com>
2021-01-31 13:01:59 +1100

    Feature (Images): Added image active storage to DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: [:show]
 
-
-  def show
-    render json: current_user
-  end
-  
   def create
     user = User.new(user_params)
     if user.save 
+      user.image.attach(params[:file])
       render status: :created
     else
       render status: :bad_request
