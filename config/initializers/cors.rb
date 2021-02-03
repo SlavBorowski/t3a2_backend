cb22a0e9d49b4c844c9f308da3f95b833acbe672
SlavBorowski <slav.borowski@outlook.com>
2021-02-03 15:33:19 +1100

    Added configuration for deployment.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'https://amazing-hopper-baec1a.netlify.app/'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
