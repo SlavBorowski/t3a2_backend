a56eb7c8de2a63340cf0de7fbf656bdc62c800ff
Andrew Downs <andrewdownsau@gmail.com>
2021-02-04 14:45:48 +1100

    Model: Validation for trip, must have unique title, date after today and city present

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,14 @@
 class Trip < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, uniqueness: true
+  validates :date, presence: true
+  validates :city, presence: true
+
+  validate :trip_date_cannot_be_in_the_past
+
+  def trip_date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past") if
+      !date.blank? and date < Date.today
+  end
 end
